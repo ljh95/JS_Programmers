ad4154fd91fcfad63b2ec8a8c7f84e49de077411
ilegwmam <ilegwmam@gmail.com>
2021-07-26 18:15:33 +0900

    순열 다시 그려보기

--- knowledges/순열조합/순열-1.js
+++ knowledges/순열조합/순열-1.js
@@ -3,8 +3,8 @@ const getCombinations = function(arr, selectNum) {
   if(selectNum === 1) return arr.map(v => [v]);
 
   arr.forEach((fixed, i) => {
-    const rest = arr.slice(i + 1);
-    // const rest = arr.filter((_, j) => i !== j);
+    // const rest = arr.slice(i + 1);
+    const rest = arr.filter((_, j) => i !== j);
     // const rest = arr;
     const combinations = getCombinations(rest, selectNum - 1);
     const attached = combinations.map(c => [fixed, ...c]);
@@ -14,5 +14,5 @@ const getCombinations = function(arr, selectNum) {
 }
 
 const example = [1, 2, 3, 4];
-const result = getCombinations(example, 3);
+const result = getCombinations(example, 4);
 console.log(result);
